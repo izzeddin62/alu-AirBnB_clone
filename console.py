20063eea8331bba80d236cf826e43f928376eec3
izzett222 <iizzeddin62@gmail.com>
2023-02-09 00:40:54 +0200

    remove init

--- console.py
+++ console.py
@@ -4,9 +4,7 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    def __init__(self):
-        cmd.Cmd.__init__(self)
-        self.prompt = '(hbnb) '
+    prompt = '(hbnb) '
     def do_quit(self, arg):
         return True
     def do_EOF(self, arg):
