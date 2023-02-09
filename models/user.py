558caae3088c3bd4a25d541f32a4ce3b987ea547
izzeddin62 <i.ishimwe@alustudent.com>
2023-02-09 16:39:45 +0200

    add class properties

--- models/user.py
+++ models/user.py
@@ -4,12 +4,12 @@ from models.base_model import BaseModel
 from models import storage
 
 class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name
 
     """initialise class"""
-    def __init__(self, email = "", password = "", first_name = "", last_name = ""):
+    def __init__(self):
         super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
         storage.new(self)
